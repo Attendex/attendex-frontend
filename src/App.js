bde48600273a3af1fd665a7a3534d86e7fc43345
foongsiqi <edu.foongs@gmail.com>
2022-01-12 12:41:50 +0800

    Add password validation in signup page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import SignIn from './pages/SignIn';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Card, TextField, Typography, Box, Button } from '@mui/material';
+
+
+function SignUpPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isMatch, setMatch] = useState(true);
+  // console.log('username', username);
+  console.log('password', password);
+  console.log('confirmPassword', confirmPassword);
+  const validatePass = () => { setMatch(password == confirmPassword)}
+  return (
+    <Box sx={{ height: '100vh', padding: '1rem' }}>
+      <Box sx={{ position: 'relative', top: '20%' }}>
+        <Card
+          elevation={3}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            margin: '0 auto',
+            padding: '1rem',
+            borderRadius: '10px',
+            maxWidth: '600px',
+            background: 'white',
+          }}
+        >
+          <Typography variant="h3" sx={{ color: 'grey' }}>
+            Sign Up
+          </Typography>
+          <TextField 
+            label="Username" 
+            variant="outlined" 
+            margin="normal" 
+            sx={{ width: '90%' }} 
+            onChange={(event) => setUsername(event.target.value)} 
+          />
+          <TextField 
+            label="Password" 
+            type="password" 
+            margin="normal" 
+            sx={{ width: '90%' }} 
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <TextField 
+            label="Confirm Password" 
+            type="password" 
+            margin="normal" 
+            sx={{width: '90%'}} 
+            error={password != confirmPassword} 
+            onChange={(event) => setConfirmPassword(event.target.value)} 
+          />
+          <Button variant="contained">Sign Up</Button>
+        </Card>
+        <Button variant="none" sx={ {margin:'1rem' }} href="/signin">Already have an account? Sign in here</Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default SignUpPage;
