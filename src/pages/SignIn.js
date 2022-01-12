f0936c0c262301dd0d4c25242d1c85c733d99566
foongsiqi <edu.foongs@gmail.com>
2022-01-12 12:07:30 +0800

    Improve styling of signin page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -3,18 +3,23 @@ import { Card, TextField, Typography, Box, Button } from '@mui/material';
 
 function SignInPage() {
   return (
-    <Box sx={{ height: '100vh' }}>
+    <Box sx={{ 
+      height: '100vh',
+      padding: '1rem'
+    }}>
       <Card
         elevation={3}
         sx={{
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
-          margin: '1rem auto',
+          margin: '0 auto',
           padding: '1rem',
           borderRadius: '10px',
           maxWidth: '600px',
           background: 'white',
+          position: 'relative',
+          top: '20%',
         }}
       >
         <Typography variant="h3" sx={{color: 'grey'}}>
